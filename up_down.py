8d3ba41dab4c80c373995cb8ec960093d63d2502
DESKTOP-MJQVKHB\user1 <pbsokok7@gmail.com>
2026-07-27 14:54:43 +0900

    추가본

--- up_down.py
+++ up_down.py
@@ -2,20 +2,27 @@ import random
 
 def up_downgame():
     while True:
+        life = 5
         x = input("나랑 게임하나 할래?(y/n)")
         if x == "y" or x == "Y":
             f_ans = (int(random.randint(1, 51)))
-            while True:
+            while True :
+                if life == 0 :
+                    print("game over!")
+                    break
                 p_ans = int(input("뭐라고 할까?(1~50)"))
                 if p_ans == f_ans:
                     print("정답이야!")
                     break
                 elif p_ans > 50 :
                     print("DOWN! 내가 1부터 50이라고 말 안했던가...?")
+                    life -= 1
                 elif p_ans > f_ans:
                     print("DOWN!")
+                    life -= 1
                 else :
                     print("UP")
+                    life -= 1
 
 
 
--- zero.py
+++ zero.py
@@ -7,7 +7,8 @@ import random
 
 def zero_game():
     while True:
-        print("you won't play? (yes or no) :")  # 게임 참여 여부
+        life = 7
+        print("우리랑 같이 게임할래? (yes or no) :\n")  # 게임 참여 여부
         play =  input()
         if play == "no" :
             print("okay bye..")
@@ -22,39 +23,52 @@ def zero_game():
                     print("roll again!")
                 elif my_roll < roll_num1 and my_roll < roll_num2:        # 공격 턴
 
-                    print("Attack!")
+                    print("Attack!\n")
                     while True:
+                        if life == 0:
+                            print("내가 졌어...")
+                            break
                         num1 = random.randint(0, 5)
                         num2 = random.randint(0, 5)
                         ans = int(input())
                         x = num1 + num2
-                        if ans == x:
-                            print(x)
-                            print("Win!")                                # 공격 승리!
+                        if ans > 10 or ans < 0:
+                            print("제대로 해!!")
+                        elif ans == x:
+                            print(f"친구들 숫자합:{x}")
+                            print("Win!\n")                                # 공격 승리!
                             break
                         else:
-                            print(x)
-                            print("Wrong!")                              # 다시 공격
+                            print(f"친구들 숫자합:{x}")
+                            print("Wrong!\n")                              # 다시 공격
+                            life -= 1
                     break
 
                 elif my_roll > roll_num1 and my_roll > roll_num2:        # 수비 턴
                     print("Defense!")
                     while True:
+                        if life == 0:
+                            print("방어 성공!!")
+                            break
                         num1 = random.randint(0, 5)
                         num2 = int(input())
-                        frind_ans = random.randint(0, 5)
+                        friend_ans = random.randint(0, 5)
                         x = num1 + num2
-                        if frind_ans == x:
-                            print(x)
-                            print(frind_ans)
+                        if num2 > 5 or num2 < 0 :
+                            print("장난 치지마!")
+                        if friend_ans == x:
+                            print(f"나의 합 :{x}")
+                            print(f"친구의 합 :{friend_ans}")
                             print("NOoooo~..")                           # 방어 실패
                             break
                         else:
-                            print(x)
-                            print(frind_ans)
-                            print("again!")                              # 방어 성공!
+                            print(f"나의 합 :{x}")
+                            print(f"친구의 합 :{friend_ans}")
+                            print("again!")   # 방어 성공!
+                            life -= 1
                     break
-
+        else :
+            print("할거야 말거야?")
 
 
 
